62ae37a2fe91232fb6855b48e65f609fff8b448d
Davide Laezza <truzzialrogo@gmx.com>
2018-05-05 10:13:44 +0200

    Added python2 fix

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 cd $(dirname $0)
-cp ./move-to-next-monitor /usr/local/bin/move-to-next-monitor
+if /usr/bin/env python -V 2>&1 | grep 'Python 2' &> /dev/null; then
+    sed '1c#!/usr/bin/env python3' ./move-to-next-monitor \
+        > /usr/local/bin/move-to-next-monitor
+else
+    cp ./move-to-next-monitor /usr/local/bin/move-to-next-monitor
+fi
