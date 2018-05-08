ec0e064d2bedc76ef65d90c97079b9e43f3c7550
Davide Laezza <truzzialrogo@gmx.com>
2018-05-08 12:24:50 +0200

    Implementing changes requested in vanaoff/move-to-next-monitor#1

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
 cd $(dirname $0)
-if /usr/bin/env python -V 2>&1 | grep 'Python 2' &> /dev/null; then
-    sed '1c#!/usr/bin/env python3' ./move-to-next-monitor \
-        > /usr/local/bin/move-to-next-monitor
-else
-    cp ./move-to-next-monitor /usr/local/bin/move-to-next-monitor
-fi
+cp ./move-to-next-monitor /usr/local/bin/move-to-next-monitor
 chmod +x /usr/local/bin/move-to-next-monitor
